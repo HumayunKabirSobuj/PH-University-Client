c95998cc0880d97351d020330726a8156920a63f
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-01-22 18:50:03 +0600

    create academic semester from create done

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -2,43 +2,65 @@ import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
+import { semesterOptions } from "../../../constants/semester";
+import { monthOptions } from "../../../constants/global";
 
-const nameOptions = [
-  {
-    value: "01",
-    label: "Autumn",
-  },
-  {
-    value: "02",
-    label: "Summer",
-  },
-  {
-    value: "03",
-    label: "Fall",
-  },
-];
+// const nameOptions = [
+//   {
+//     value: "01",
+//     label: "Autumn",
+//   },
+//   {
+//     value: "02",
+//     label: "Summer",
+//   },
+//   {
+//     value: "03",
+//     label: "Fall",
+//   },
+// ];
+
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
+console.log(yearOptions);
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
-    const name = nameOptions[Number(data?.name) - 1].label;
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
     const semesterData = {
       name: name,
       code: data?.name,
+      year: data?.year,
+      startMonth: data?.startMonth,
+      endMonth: data?.endMonth,
     };
     console.log(semesterData);
   };
 
   return (
-    <Flex justify="center" align="center" >
+    <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHSelect label="Name" name={"name"} options={nameOptions} />
-          <PHSelect label="Year" name={"year"} options={nameOptions} />
-          <PHSelect label="Start Month" name={"startMonth"} options={nameOptions} />
-          <PHSelect label="End Month" name={"endMonth"} options={nameOptions} />
+          <PHSelect label="Name" name={"name"} options={semesterOptions} />
+          <PHSelect label="Year" name={"year"} options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name={"startMonth"}
+            options={monthOptions}
+          />
+          <PHSelect
+            label="End Month"
+            name={"endMonth"}
+            options={monthOptions}
+          />
           <Flex justify="center" align="center">
-            <Button type="primary" style={{width:"100%"}} htmlType="submit">Submit</Button>
+            <Button type="primary" style={{ width: "100%" }} htmlType="submit">
+              Submit
+            </Button>
           </Flex>
         </PHForm>
       </Col>
